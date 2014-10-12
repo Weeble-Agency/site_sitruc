afa38c44c726b9e24254bf30acc195790c66df6b
Valentin <valentin.levalet@live.fr>
2014-10-12 21:55:52 +0200

    Ajout base du site

---
+++ js/data.js
@@ -0,0 +1,10 @@
+var codropsEvents = {
+	'05-22-2014' : '<span>Evènement 1</span>',
+	'05-23-2014' : '<span>Evènement 2</span>',
+	'05-28-2014' : '<span>Evènement 3</span>',
+	'05-29-2014' : '<span>Evènement 4</span>',
+	'06-05-2014' : '<span>Evènement 5</span>',
+	'06-08-2014' : '<span>Evènement 6</span>',
+	'06-10-2014' : '<span>Evènement 7</span>',
+	'06-16-2014' : '<span>Evènement 8</span>',
+};
